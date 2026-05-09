e28d664f20439cca58caa41122ddb5683c270bed
Rrerr <jamesjunto22@gmail.com>
2026-05-09 13:50:49 +0800

    add map and show current location

--- livetrack/src/components/MainContainer.jsx
+++ livetrack/src/components/MainContainer.jsx
@@ -1,7 +1,74 @@
-const MainContainer = () =>{
-    return(
-        <>
-        
-        </>
-    )
+import { MapContainer, TileLayer } from 'react-leaflet';
+import { useState, useEffect } from 'react';
+import 'leaflet/dist/leaflet.css';
+
+const MainContainer = () => {
+
+  const [location, setLocation] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+
+    if (navigator.geolocation) {
+
+      navigator.geolocation.getCurrentPosition(
+
+        (position) => {
+
+          setLocation([
+            position.coords.latitude,
+            position.coords.longitude
+          ]);
+
+        },
+
+        (err) => {
+          setError(err.message);
+        },
+        {
+          enableHighAccuracy: true
+        }
+
+      );
+
+
+    } else {
+
+      setError("Geolocation is not supported by this browser.");
+
+    }
+
+  }, []);
+
+  console.log(location)
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!location) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div className="h-screen w-full">
+
+      <MapContainer
+        center={location}
+        zoom={13}
+        className="h-full w-full"
+      >
+
+        <TileLayer
+          attribution='&copy; OpenStreetMap contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+      </MapContainer>
+
+    </div>
+  );
+
 }
+
+export default MainContainer;

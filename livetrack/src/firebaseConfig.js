ab84701945b3278378c0b76034113bd1e7464f5b
Rrerr <jamesjunto22@gmail.com>
2026-05-10 23:30:38 +0800

    setting up firebase

---
+++ livetrack/src/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjk9H4UioWdhlYBBptXw2tG8aA0VS9ZtU",
+  authDomain: "livetrack-65cd7.firebaseapp.com",
+  projectId: "livetrack-65cd7",
+  storageBucket: "livetrack-65cd7.firebasestorage.app",
+  messagingSenderId: "521863899026",
+  appId: "1:521863899026:web:566666a985b2b66340c34b"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
